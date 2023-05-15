dffacf46db186775fc1e58c96da2890ffa7e07b8
EvaldasZak <evaldas.zaksas@gmail.com>
2023-05-15 18:08:58 +0300

    UserProvider and logingIn

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
+import { useContext } from 'react';
+
+import UsersContext from './context/UsersContext';
 
 import Home from './components/Pages/Home';
 import Register from './components/Pages/Register';
@@ -7,13 +10,14 @@ import Login from './components/Pages/Login';
 
 
 const App = () => {
-  const user = null;
+  const { currentUser } = useContext(UsersContext);
+
 
   return (
     <>
     <Routes>
       {
-        !user ? (
+        !currentUser ? (
           <>
           <Route index element={<Login />} />
           <Route path='/register'>
--- src/components/Pages/Login.jsx
+++ src/components/Pages/Login.jsx
@@ -1,47 +1,64 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
+import {useNavigate} from 'react-router-dom'
+
+import UsersContext from '../../context/UsersContext'
 
 const Login = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+    const {users, setCurrentUser, currentUser} = useContext(UsersContext)
+    const navigate = useNavigate();
+    
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    
+    const handleUsernameChange = (e) => {
+        setUsername(e.target.value);
+    };
+    
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value);
+    };
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
 
-  const handleUsernameChange = (e) => {
-    setUsername(e.target.value);
-  };
+        const logedInUser = users.find(user => username === user.email && password === user.password)
+        if (logedInUser) {
+            setCurrentUser(logedInUser)
+            navigate('/')
 
-  const handlePasswordChange = (e) => {
-    setPassword(e.target.value);
-  };
+            setUsername('');
+            setPassword('');
+        } else {
+            setPassword('');
+            alert('Login unsuccessful!');
+        }
+        
+        // Perform login logic here
+        // You can make an API request to authenticate the user or handle it as per your requirements
+        
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    // Perform login logic here
-    // You can make an API request to authenticate the user or handle it as per your requirements
-
-    alert('Login successful!');
-    setUsername('');
-    setPassword('');
-  };
-
-  return (
-    <div>
-      <h2>Login</h2>
-      <form onSubmit={handleSubmit}>
+    };
+    
+    return (
         <div>
-          <label>Username:</label>
-          <input type="text" value={username} onChange={handleUsernameChange} />
+        <h2>Login</h2>
+        <form onSubmit={handleSubmit}>
+        <div>
+        <label>Username:</label>
+        <input type="text" value={username} onChange={handleUsernameChange} />
         </div>
         <div>
-          <label>Password:</label>
-          <input type="password" value={password} onChange={handlePasswordChange} />
+        <label>Password:</label>
+        <input type="password" value={password} onChange={handlePasswordChange} />
         </div>
         <button type="submit">Login</button>
-      </form>
-      <a href="/register">
+        </form>
+        <a href="/register">
         Already registered
-      </a>
-    </div>
-  );
-};
-
-export default Login;
+        </a>
+        </div>
+        );
+    };
+    
+    export default Login;
+    
