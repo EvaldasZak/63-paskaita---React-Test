5bdfdc0b439d6cab8e1e5b8ac2a62776a8ced6b6
EvaldasZak <evaldas.zaksas@gmail.com>
2023-05-15 16:23:49 +0300

    Router added, home page init

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+
+import Home from './components/Pages/Home';
+
 
 const App = () => {
   return (
     <>
-    <h1>Pradžia</h1>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path='*' element={<h1>Error 404</h1>} />
+      </Routes>
     </>
   );
 }
