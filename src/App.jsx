8d56769ffbab13b60b25c992d5a468ea668c6ebb
EvaldasZak <evaldas.zaksas@gmail.com>
2023-05-15 16:35:26 +0300

    Register, login pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,19 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 
 import Home from './components/Pages/Home';
+import Register from './components/Pages/Register';
+import Login from './components/Pages/Login';
 
 
 const App = () => {
   return (
     <>
       <Routes>
-        <Route index element={<Home />} />
+        <Route index element={<Register />} />
+        <Route path='/login'>
+          <Route index element={<Login />} />
+        </Route>
+        {/* <Route index element={<Home />} /> */}
         <Route path='*' element={<h1>Error 404</h1>} />
       </Routes>
     </>
